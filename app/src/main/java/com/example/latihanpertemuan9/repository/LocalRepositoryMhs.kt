b9f0e8760ab9a6a34f3f647ccf6db90c53a1f774
Rauda08 <raudajnnh08@gmail.com>
2024-12-16 01:53:14 +0700

    mengimplementasi fungsi get, insert, delete, update

---
+++ app/src/main/java/com/example/latihanpertemuan9/repository/LocalRepositoryMhs.kt
@@ -0,0 +1,30 @@
+package com.example.latihanpertemuan9.repository
+
+import com.example.latihanpertemuan9.data.dao.MahasiswaDao
+import com.example.latihanpertemuan9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryMhs(
+    private val mahasiswaDao: MahasiswaDao
+) : RepositoryMhs {
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMahasiswa(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+
+}
